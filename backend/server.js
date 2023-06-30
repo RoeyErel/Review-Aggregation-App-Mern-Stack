61c3b9ef3c746761922317497ae669960bbd758a
RoeyErel <roee42@gmail.com>
2023-06-30 04:33:09 +0300

    3.2 production

--- backend/server.js
+++ backend/server.js
@@ -22,6 +22,12 @@ app.use(
 // Routes
 app.use('/api/users', userRoute)
 app.use('/api/lists', listRoute)
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, PUT, POST");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 app.get("/", (req, res) => {
   res.send("Connected");
 });
